f0c6222e2e4776821dee1b523411bb045897af53
lX57 <36733349+K-Yoshizawa@users.noreply.github.com>
2024-12-14 20:51:04 +0900

    Dijkstra (途中)

--- Library/Common.hpp
+++ Library/Common.hpp
@@ -23,4 +23,18 @@
 #include <tuple>
 #include <utility>
 #include <vector>
+#include <type_traits>
 using namespace std;
+
+template<typename T>
+T get_INF(){
+    if constexpr (is_integral_v<T>){
+        return numeric_limits<T>::max() >> 2;
+    }
+    else if constexpr (is_arithmetic_v<T>){
+        return numeric_limits<T>::max() / 4.0;
+    }
+    else{
+        return T();
+    }
+}
--- Library/Graph/Dijkstra.hpp
+++ Library/Graph/Dijkstra.hpp
@@ -1,105 +1,70 @@
 /**
  * @file Dijkstra.hpp
  * @brief Dijkstra - ダイクストラ法
- * @version 4.0
- * @date 2024-09-01
+ * @date 2024-12-12
  */
 
-#include "Graph.hpp"
+#include "../Common.hpp"
 
-template<typename CostType>
+using Vertex = int;
+
+template<typename WeightType, bool isDirected = false>
 class Dijkstra{
-    public:
-    Dijkstra(Graph<CostType> &G) : 
-            graph_(G), inf_(G.get_inf()), dist_(G.get_vertex_size(), inf_),
-            prev_vertex_(G.get_vertex_size(), -1){}
+    private:
+    vector<vector<pair<Vertex, WeightType>>> graph;
+    vector<WeightType> dist;
+    vector<Vertex> prev_vertex;
+    WeightType INF = get_INF<WeightType>();
 
-    /**
-     * @brief 頂点 `source` から他の全頂点への最短距離を求める。
-     * @note 到達不能の場合、`inf` となる。
-     * @param source 始点の頂点
-     */
+    public:
+    Dijkstra(int vertex_size) : graph(vertex_size), dist(vertex_size), prev_vertex(vertex_size){}
+    
+    void AddEdge(Vertex from, Vertex to, WeightType weight){
+        graph.at(from).emplace_back(to, weight);
+        if(!isDirected){
+            graph.at(to).emplace_back(from, weight);
+        }
+    }
+
     void Solve(Vertex source){
-        graph_.Validate(source);
-        if(source_vertex_ == source) return;
-        dist_.assign(dist_.size(), inf_);
-        prev_vertex_.assign(prev_vertex_.size(), -1);
-        dist_[source] = 0;
-        using P = pair<CostType, Vertex>;
+        fill(dist.begin(), dist.end(), INF);
+        fill(prev_vertex.begin(), prev_vertex.end(), -1);
+        dist.at(source) = WeightType();
+        using P = pair<WeightType, Vertex>;
         priority_queue<P, vector<P>, greater<P>> que;
-        que.emplace(0, source);
-        while(que.size()){
-            auto [d, v] = que.top(); que.pop();
-            if(dist_[v] != d) continue;
-            for(Edge<CostType> &e : graph_[v]){
-                CostType nd = d + e.cost;
-                if(nd < dist_[e.to]){
-                    dist_[e.to] = nd;
-                    prev_vertex_[e.to] = v;
-                    que.emplace(nd, e.to);
+        que.emplace(WeightType(), source);
+        while(!que.empty()){
+            auto [d, u] = que.top(); que.pop();
+            if(dist.at(u) != d) continue;
+            for(auto [v, w] : graph.at(u)){
+                if(dist.at(v) > d + w){
+                    dist.at(v) = d + w;
+                    prev_vertex.at(v) = u;
+                    que.emplace(d + w, v);
                 }
             }
         }
-        source_vertex_ = source;
     }
 
-    /**
-     * @brief 頂点 `source` から頂点 `target` に到達可能かを返す。
-     * @attention 予め始点の頂点 `source` について `Solve()` を呼び出している必要がある。
-     * @param target 終点の頂点
-     * @return true 到達可能
-     * @return false 到達不能
-     */
+    WeightType Distance(Vertex target) const {
+        return dist.at(target);
+    }
+
     bool Reachable(Vertex target) const {
-        graph_.Validate(target);
-        return dist_[target] != inf_;
+        return dist.at(target) != INF;
     }
 
-    /**
-     * @brief 頂点 `source` から頂点 `target` への最短パスを返す。
-     * @attention 予め始点の頂点 `source` について `Solve()` を呼び出している必要がある。
-     * @attention 頂点 `target` に到達不能の場合、空列を返す。
-     * @param target 終点の頂点
-     * @return vector<Vertex> 最短パスに含まれる頂点列を `source` から `target` の順に並べたもの。到達不能の場合は空列が返される。
-     */
-    vector<Vertex> Restore(Vertex target) const {
-        graph_.Validate(target);
-        if(!Reachable(target)) return vector<Vertex>{};
-        vector<Vertex> ret{target};
+    vector<tuple<Vertex, Vertex, WeightType>> Restore(Vertex target){
+        vector<tuple<Vertex, Vertex, WeightType>> res;
+        if(!Reachable(target)) return res;
         Vertex v = target;
-        while(v != source_vertex_){
-            v = prev_vertex_[v];
-            ret.push_back(v);
+        while(prev_vertex.at(v) != -1){
+            int u = prev_vertex.at(v);
+            WeightType w = dist.at(v) - dist.at(u);
+            res.emplace_back(u, v, w);
+            v = u;
         }
-        reverse(ret.begin(), ret.end());
-        return ret;
+        reverse(res.begin(), res.end());
+        return res;
     }
-
-    /**
-     * @brief 頂点 `source` から頂点 `target` への最短経路長を返す。
-     * @attention 予め始点の頂点 `source` について `Solve()` を呼び出している必要がある。
-     * @attention 到達不能の場合は `inf` が返される。
-     * @param target 終点の頂点
-     * @return CostType 最短経路長
-     */
-    CostType Distance(Vertex target) const {
-        graph_.Validate(target);
-        return dist_[target];
-    }
-
-    CostType operator[](Vertex target){
-        return Distance(target);
-    }
-
-    const CostType operator[](Vertex target) const {
-        return Distance(target);
-    }
-
-    private:
-    Graph<CostType> &graph_;
-
-    CostType inf_;
-    vector<CostType> dist_;
-    vector<Vertex> prev_vertex_;
-    Vertex source_vertex_{-1};
 };
